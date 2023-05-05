ef1d0a125400da25a735bfd38a47b491617a433c
j-segura <juanda.segura2005@gmail.com>
2023-05-05 07:06:27 -0500

    solve mistakes

--- SQL_JOINS/practice.sql
+++ SQL_JOINS/practice.sql
@@ -13,18 +13,18 @@ CREATE TABLE costumers(
 
 GO
 
-CREATE TABLE appointments(
+CREATE TABLE doctors(
     id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    fecha DATETIME NOT NULL,
-    costumer_id INT FOREIGN KEY REFERENCES costumers(id) NOT NULL
-    doctor_id INT FOREIGN KEY REFERENCES doctors(id) NOT NULL
+    nom VARCHAR(100) NOT NULL,
 )
 
 GO
 
-CREATE TABLE doctors(
+CREATE TABLE appointments(
     id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    nom VARCHAR(100) NOT NULL,
+    fecha DATETIME NOT NULL,
+    costumer_id INT FOREIGN KEY REFERENCES costumers(id) NOT NULL,
+    doctor_id INT FOREIGN KEY REFERENCES doctors(id) NOT NULL
 )
 
 GO
@@ -33,7 +33,7 @@ INSERT INTO costumers(id, nom) VALUES(100, 'Carlos'),
     (101, 'Maria'),
     (102, 'Sandra'),
     (103, 'Monica'),
-    (104, 'Leidy'),
+    (104, 'Leidy')
 
 GO
 
@@ -41,9 +41,9 @@ INSERT INTO doctors VALUES('Juan'),
     ('Matias'),
     ('Esteban')
 
-INSERT INTO appointments(fecha, costumer_id, doctor_id) VALUES('11/11/2023' 100, 1),
-    ('12/12/2022' 100, 1),
-    ('11/10/2022' 100, 1),
-    ('10/10/2023' 100, 1),
+INSERT INTO appointments(fecha, costumer_id, doctor_id) VALUES('11/11/2023', 100, 1),
+    ('12/12/2022', 102, 1),
+    ('11/10/2022', 103, 3),
+    ('10/10/2023', 104, 1)
 
 
